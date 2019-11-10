a7a448a0a0a0e7c24bb24a229f0756956ca5b1db
Nicholas Muturi <nicholasmuturi1@gmail.com>
2019-11-10 19:42:49 +0300

    [chore] add floating action button to main page, will be used to add new recipes

--- app/src/main/java/m/nicholas/mealville/Adapters/myRecipeListAdapter.java
+++ app/src/main/java/m/nicholas/mealville/Adapters/myRecipeListAdapter.java
@@ -1,13 +1,17 @@
 package m.nicholas.mealville.Adapters;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import m.nicholas.mealville.R;
 import m.nicholas.mealville.models.Recipe;
 
 public class myRecipeListAdapter extends ArrayAdapter<Recipe> {
@@ -34,6 +38,27 @@ public class myRecipeListAdapter extends ArrayAdapter<Recipe> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        String mealTitle = getItem(position).getMealTitle();
+        String description = getItem(position).getDescription();
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View listView;
+
+        if(convertView == null){
+            listView = inflater.inflate(R.layout.single_recipe_list_item,null);
+            TextView tvMealTitle = listView.findViewById(R.id.tvMealTitle);
+            TextView tvMealDescription = listView.findViewById(R.id.tvMealDescription);
+            tvMealTitle.setText(mealTitle);
+            tvMealDescription.setSingleLine(true);
+            tvMealDescription.setEllipsize(TextUtils.TruncateAt.END);
+            tvMealDescription.setText(description);
+
+        } else {
+            listView = convertView;
+        }
+        return listView;
     }
+
+
+
 }
--- app/src/main/java/m/nicholas/mealville/MainActivity.java
+++ app/src/main/java/m/nicholas/mealville/MainActivity.java
@@ -4,18 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+import m.nicholas.mealville.Adapters.myRecipeListAdapter;
+import m.nicholas.mealville.models.Recipe;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.mainRecipesList) ListView listView;
+    @BindView(R.id.mainRecipesList) ListView recipeListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        List<String> testIngredients = new ArrayList<>();
+        List<String> testSteps = new ArrayList<>();
+        testIngredients.add("Dhania");
+        testIngredients.add("Pilau Masala");
+        testSteps.add("Boil rice");
+        testSteps.add("Cut Onions");
+        Recipe testRecipe = new Recipe("Pilau", "Amazing Dish",testIngredients,testSteps);
+        myRecipeListAdapter recipeListAdapter = new myRecipeListAdapter(this,R.layout.single_recipe_list_item,testRecipe);
+        recipeListView.setAdapter(recipeListAdapter);
 
     }
 
