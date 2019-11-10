6149617a6e76cf5f37a9ae0b079d112a1e501813
Nicholas Muturi <nicholasmuturi1@gmail.com>
2019-11-10 19:50:40 +0300

    [chore] attached click listener to floatingActionButton

--- app/src/main/java/m/nicholas/mealville/MainActivity.java
+++ app/src/main/java/m/nicholas/mealville/MainActivity.java
@@ -3,11 +3,14 @@ package m.nicholas.mealville;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,7 @@ import m.nicholas.mealville.models.Recipe;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.mainRecipesList) ListView recipeListView;
+    @BindView(R.id.floatingActionButton) FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +38,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Recipe testRecipe = new Recipe("Pilau", "Amazing Dish",testIngredients,testSteps);
         myRecipeListAdapter recipeListAdapter = new myRecipeListAdapter(this,R.layout.single_recipe_list_item,testRecipe);
         recipeListView.setAdapter(recipeListAdapter);
-
+        fab.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        if(view == fab) {
 
+        }
     }
 }
